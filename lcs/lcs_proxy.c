f1accf024997305fd988f36513ab81a92de45ab2
richblomseth <rich.blomseth@gmail.com>
2023-01-25 22:34:54 -0800

    Moved include files to header file

--- lcs/lcs_proxy.c
+++ lcs/lcs_proxy.c
@@ -31,15 +31,6 @@
 //
 
 #ifdef ENABLE_PROXY_REPEATING
-#include <stddef.h>
-#include <string.h>
-#include <stdio.h>
-#include "IzotTypes.h"
-#include "lcs_eia709_1.h"
-#include "lcs_node.h"
-#include "lcs_app.h"
-#include "lcs_api.h"
-#include "lcs_netmgmt.h"
 #include "lcs_proxy.h"
 
 Status ProcessLTEP(APPReceiveParam *appReceiveParamPtr, APDU *apduPtr);
