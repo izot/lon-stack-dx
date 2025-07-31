bb72606237824da67e3f2aab6c0ff93352928ec9
Rich Blomseth <rich.blomseth@enocean.com>
2025-07-31 15:02:40 -0700

    Update date

--- include/isi/isi_int.h
+++ include/isi/isi_int.h
@@ -2,7 +2,7 @@
 //
 // isi_int.h
 //
-// Copyright (C) 2022 EnOcean
+// Copyright (C) 2022-2025 EnOcean
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in 
