99247c9d536436a70c559327b022e657a2c3b09c
Rich Blomseth <rich.blomseth@enocean.com>
2025-08-08 14:52:54 -0700

    Initial check-in

---
+++ aarch64-toolchain.cmake
@@ -0,0 +1,19 @@
+# aarch64-toolchain.cmake
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+#set(CMAKE_C_COMPILER /opt/homebrew/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_C_COMPILER /opt/homebrew/bin/aarch64-linux-gnu-gcc CACHE FILEPATH "" FORCE)
+set(CMAKE_C_COMPILER_FORCED TRUE CACHE BOOL "" FORCE)
+set(CMAKE_CXX_COMPILER /opt/homebrew/bin/aarch64-linux-gnu-g++)
+
+# Optionally, set the sysroot
+#set(CMAKE_SYSROOT /path/to/raspbian/sysroot)
+#set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Optionally, remove Mac-specific -arch arm64 from all relevant flag variables.
+string(REGEX REPLACE "-arch[ ]*arm64" "" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+string(REGEX REPLACE "-arch[ ]*arm64" "" CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT}")
+string(REGEX REPLACE "-arch[ ]*arm64" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+string(REGEX REPLACE "-arch[ ]*arm64" "" CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}")
