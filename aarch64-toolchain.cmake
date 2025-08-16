87a53b453dc27fbd43c572af21693f7c1d12023f
Rich Blomseth <rich.blomseth@enocean.com>
2025-08-16 13:20:36 -0700

    Add aarch64 sysroot

--- aarch64-toolchain.cmake
+++ aarch64-toolchain.cmake
@@ -6,13 +6,15 @@ set(CMAKE_C_COMPILER /opt/homebrew/bin/aarch64-linux-gnu-gcc CACHE FILEPATH "" F
 set(CMAKE_C_COMPILER_FORCED TRUE CACHE BOOL "" FORCE)
 set(CMAKE_CXX_COMPILER /opt/homebrew/bin/aarch64-linux-gnu-g++)
 
-# Optionally, set the sysroot
-#set(CMAKE_SYSROOT /path/to/raspbian/sysroot)
-#set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+# Set the sysroot for correct target headers and libraries
+set(CMAKE_SYSROOT /opt/homebrew/opt/aarch64-unknown-linux-gnu/toolchain/aarch64-unknown-linux-gnu/sysroot)
 
-# Optionally, remove Mac-specific -arch arm64 from all relevant flag variables.
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Remove Mac-specific -arch arm64 from all relevant flag variables
 string(REGEX REPLACE "-arch[ ]*arm64" "" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
 string(REGEX REPLACE "-arch[ ]*arm64" "" CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT}")
 string(REGEX REPLACE "-arch[ ]*arm64" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
